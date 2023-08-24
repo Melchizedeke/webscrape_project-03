ac38b9e9e00fa6446efcb6a1e7168e67a5f5d130
Igoche John Eigege <140503808+Melchizedeke@users.noreply.github.com>
2023-08-24 12:31:25 +0100

    new codes
    succussfully looped through the colletions soup object and saved the them to the book details list object

---
+++ main.py
@@ -0,0 +1,24 @@
+# Basic Libraries Import
+import requests
+from bs4 import BeautifulSoup
+
+# Making a request to the website
+res = requests.get("https://www.abebooks.com/collections/mc/first-editions/53jKjLpLq5krU0qFS5tvnO?cm_sp=ccbrowse-_-p0-_-collections")
+site_text = res.text
+soup = BeautifulSoup(site_text, "html.parser")
+
+# Getting every book's information and saving it in a collections list uaing the `find_all()` method.
+collections = soup.find_all(class_="collection-item")
+
+# Loop through the collections lust and save items in a library
+book_details = []
+
+for item in collections:
+    book_details.append({
+        "Book_Title":item.find(class_="title").getText(),
+        "Book_Author":item.find(class_="authors").getText(),
+        "Pub-Year":item.find(class_="published-year").getText(),
+        "Price($)":item.find(class_="price").getText()
+    })
+
+print(book_details)
